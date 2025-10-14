29c2a265a8df20485d9c2b369f0df183dddf1ba3
prabhatsingh03 <prabhat.singh@adventz.com>
2025-10-14 10:03:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -412,5 +412,6 @@ def delete_subcontractor(subcontractor_id: int):
 
 if __name__ == '__main__':
     setup_database(app)
-app.run(debug=True, host='0.0.0.0', port=5000)
+app.run(debug=False, host='0.0.0.0', port=5000)
+
 
